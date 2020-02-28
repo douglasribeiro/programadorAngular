9d671feb00a79692ea648a114112c612809b22b0
douglas.silva <douglas.silva@senior.com.br>
2020-02-28 15:23:23 -0300

    commit album

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MusicaComponent } from './musica/musica/musica.component';
 import { InterpreteComponent } from './interprete/interprete/interprete.component';
+import { AlbumComponent } from './album/album/album.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MusicaComponent,
-    InterpreteComponent
+    InterpreteComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
